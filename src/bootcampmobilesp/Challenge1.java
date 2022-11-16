830516de11348258f7149300249c262845ded915
Chriztian Manzano <cristianorb@unicauca.edu.co>
2022-11-16 15:03:10 -0500

    feat: complete method for generate a String of random number

--- src/bootcampmobilesp/Challenge1.java
+++ src/bootcampmobilesp/Challenge1.java
@@ -15,7 +15,27 @@ public class Challenge1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(generateRandom("TipoA"));
+        System.out.println(generateRandom("TipoB"));
+        System.out.println(generateRandom("TipoC"));
+        
+    }
+    
+    public static String generateRandom(String type){
+        /**
+         * This method returns a string of 10 character composed only of numbers.    
+         */
+        String answer = "";
+        if(type.compareTo("TipoA") == 0 || type.compareTo("TipoB") == 0){
+            Integer randomGenerate = (int)(Math.random()*99999999+1);
+            String randomToString = randomGenerate.toString();
+            answer = (type.compareTo("TipoA") == 0)? "54" :"08";
+            for(int i=0; i<8-randomToString.length();i++){
+                answer += "0";
+            }
+            answer += randomToString;
+        }
+        return answer;
     }
     
 }
