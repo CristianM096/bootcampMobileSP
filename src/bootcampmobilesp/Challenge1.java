962682af3726d28d3ac9b46a69987319c50c14f2
Chriztian Manzano <cristianorb@unicauca.edu.co>
2022-11-16 15:24:31 -0500

    feat: Number sorter finished with Merge-Sort sorting

--- src/bootcampmobilesp/Challenge1.java
+++ src/bootcampmobilesp/Challenge1.java
@@ -15,10 +15,18 @@ public class Challenge1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println(generateRandom("TipoA"));
-        System.out.println(generateRandom("TipoB"));
-        System.out.println(generateRandom("TipoC"));
+        String order = generateRandom("TipoA");
+        System.out.println(order);
+        String[] listOrder = order.split("");
+        int[] listOrder2 = new int[10];
+        for (int i = 0; i < listOrder2.length; i++) {
+            listOrder2[i] = Integer.parseInt(listOrder[i]);
+        }
+        mergeSort(listOrder2, 0, 9);
         
+        for (int ord : listOrder2) {
+            System.out.print(ord);
+        }
     }
     
     public static String generateRandom(String type){
@@ -36,6 +44,67 @@ public class Challenge1 {
             answer += randomToString;
         }
         return answer;
+    }
+    
+    public static void merge(int array[], int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int L[] = new int[n1];
+        int M[] = new int[n2];
+
+        // fill the left and right array
+        for (int i = 0; i < n1; i++)
+            L[i] = array[p + i];
+        for (int j = 0; j < n2; j++)
+            M[j] = array[q + 1 + j];
+        // Maintain current index of sub-arrays and main array
+        int i, j, k;
+        i = 0;
+        j = 0;
+        k = p;
+        // Until we reach either end of either L or M, pick larger among
+        // elements L and M and place them in the correct position at A[p..r]
+        // for sorting in descending
+        // use if(L[i] >= <[j])
+        while (i < n1 && j < n2) {
+            if (L[i] <= M[j]) {
+                array[k] = L[i];
+                i++;
+            } else {
+                array[k] = M[j];
+                j++;
+            }
+            k++;
+        }
+        // When we run out of elements in either L or M,
+        // pick up the remaining elements and put in A[p..r]
+        while (i < n1) {
+            array[k] = L[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            array[k] = M[j];
+            j++;
+            k++;
+        }
+    }
+
+  // Divide the array into two sub arrays, sort them and merge them
+    public static void mergeSort(int array[], int left, int right) {
+        if (left < right) {
+
+            // m is the point where the array is divided into two sub arrays
+            int mid = (left + right) / 2;
+
+            // recursive call to each sub arrays
+            mergeSort(array, left, mid);
+            mergeSort(array, mid + 1, right);
+
+            // Merge the sorted sub arrays
+            merge(array, left, mid, right);
+        }
     }
     
 }
